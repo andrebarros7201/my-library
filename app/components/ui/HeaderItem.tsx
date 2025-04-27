f0f99e1492087c7a29fb5ef08dd232775b77fcf4
André Barros <andrebarros7201@gmail.com>
2025-04-27 20:28:17 +0100

    Add ease-in-out for smoother hover transition
    Updated the hover transition style in HeaderItem to include `ease-in-out`. This enhances the user experience by providing a smoother visual effect during the transition.

--- app/components/ui/HeaderItem.tsx
+++ app/components/ui/HeaderItem.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const style =
-  "px-4 py-2 rounded-md bg-blue-500 text-white shadow-md hover:bg-blue-700 transition-all duration-300 cursor-pointer";
+  "px-4 py-2 rounded-md bg-blue-500 text-white shadow-md hover:bg-blue-700 transition-all duration-300 ease-in-out cursor-pointer";
 
 const HeaderItem = ({ label, href, onClick }: Props) => {
   return href ? (
