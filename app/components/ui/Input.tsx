25f52544129bed8429d1992d2568dd09ed127872
André Barros <andrebarros7201@gmail.com>
2025-04-27 20:35:52 +0100

    Add reusable Input component with customizable props
    Introduced a forwardRef-based Input component to streamline form inputs. It supports various types, constraints, and accessibility through flexible props like label, id, type, and validation options. This addition enhances code reusability and consistency across the UI.

---
+++ app/components/ui/Input.tsx
@@ -0,0 +1,58 @@
+import { forwardRef } from "react";
+
+type Props = {
+  label: string;
+  type: "text" | "password" | "number";
+  id: string;
+  minValue?: number;
+  maxValue?: number;
+  minLength?: number;
+  maxLength?: number;
+  required?: boolean;
+  placeholder?: string;
+};
+
+const Input = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      label,
+      type,
+      maxLength,
+      minLength,
+      minValue,
+      maxValue,
+      required,
+      id,
+      placeholder,
+    },
+    ref,
+  ) => {
+    return (
+      <div className="flex flex-col w-full gap-4">
+        <label htmlFor={id}>{label}</label>
+        <input
+          className={
+            "w-full px-4 py-2 border-b-2 border-b-transparent rounded-md focus:outline-none focus:border-b-blue-500 transition-all duration-300 ease-in-out text-blue-500"
+          }
+          ref={ref}
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          required={required}
+          {...(type !== "number"
+            ? {
+                minLength: minLength,
+                maxLength: maxLength,
+              }
+            : {
+                min: minValue,
+                max: maxValue,
+              })}
+        />
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
+export default Input;
