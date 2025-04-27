fa61c4c677b5b3fa00a98506b13df367bd15e7b7
André Barros <andrebarros7201@gmail.com>
2025-04-27 20:09:32 +0100

    Add reusable HeaderItem component and integrate into Header
    Introduced a new `HeaderItem` component for reusable navigation links or buttons with consistent styling. Updated the `Header` component to include sign-up and log-in links using the new `HeaderItem`. This improves code modularity and maintainability.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,16 @@
+import HeaderItem from "@/app/components/ui/HeaderItem";
+
 const Header = () => {
   return (
     <header className={"w-full flex justify-center items-center"}>
       <div
         className={
-          "w-full max-w-6xl p-4 border-2 border-green-500 flex justify-start"
+          "w-full max-w-6xl p-4 border-2 border-green-500 flex justify-start items-center gap-4"
         }
       >
         <h3 className={"font-bold text-2xl"}>My Library</h3>
+        <HeaderItem label={"Sign Up"} href={"/signup"} />
+        <HeaderItem label={"Log In"} href={"/login"} />
       </div>
     </header>
   );
---
+++ app/components/ui/HeaderItem.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+type Props = {
+  label: string;
+  href?: string;
+  onClick?: () => void;
+};
+
+const style =
+  "px-4 py-2 rounded-md bg-blue-500 text-white shadow-md hover:bg-blue-700 transition-all duration-300 cursor-pointer";
+
+const HeaderItem = ({ label, href, onClick }: Props) => {
+  return href ? (
+    <Link className={style} href={href}>
+      {label}
+    </Link>
+  ) : (
+    <button className={style} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+export default HeaderItem;
