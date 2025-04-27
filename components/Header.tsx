3cb69d32d77bd93d3abbc109519c2f0926328e6c
André Barros <andrebarros7201@gmail.com>
2025-04-27 21:12:50 +0100

    Update Header layout to include a Home link and better spacing
    Reorganized the Header component to add a "Home" link and adjust spacing between elements. This ensures improved alignment and a clearer structure, enhancing usability and navigation.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,18 @@ const Header = () => {
   return (
     <header className={"w-full flex justify-center items-center"}>
       <div
-        className={"w-full max-w-6xl p-4 flex justify-start items-center gap-4"}
+        className={
+          "w-full max-w-6xl p-4 flex justify-between items-center gap-4"
+        }
       >
         <h3 className={"font-bold text-2xl"}>My Library</h3>
-        <HeaderItem label={"Sign Up"} href={"/signup"} />
-        <HeaderItem label={"Log In"} href={"/login"} />
+        <div className={"flex gap-4"}>
+          <HeaderItem label={"Home"} href={"/"} />
+        </div>
+        <div className={"flex gap-4"}>
+          <HeaderItem label={"Sign Up"} href={"/signup"} />
+          <HeaderItem label={"Log In"} href={"/login"} />
+        </div>
       </div>
     </header>
   );
