eb8132fa43230a82c4c25eb82542fba703e6c297
André Barros <andrebarros7201@gmail.com>
2025-04-27 21:12:40 +0100

    Update Input component styles and label formatting
    Enhanced the Input component by adding a larger font for medium screens, capitalizing the label, and aligning it to the left. Adjusted the input background to a light gray for better contrast and usability. These changes improve the overall design clarity and user experience.

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -28,11 +28,13 @@ const Input = forwardRef<HTMLInputElement, Props>(
     ref,
   ) => {
     return (
-      <div className="flex flex-col items-start justify-start w-full gap-4">
-        <label htmlFor={id}>{label}</label>
+      <div className="flex flex-col items-start justify-start w-full gap-4 md:text-2xl">
+        <label className={"capitalize w-full text-left"} htmlFor={id}>
+          {label}
+        </label>
         <input
           className={
-            "w-full px-4 py-2 border-b-2 border-b-transparent rounded-md focus:outline-none focus:border-b-blue-500 transition-all duration-300 ease-in-out text-blue-500"
+            "w-full px-4 py-2 border-b-2 border-b-transparent bg-gray-200 focus:outline-none focus:border-b-blue-500 transition-all duration-300 ease-in-out text-blue-500"
           }
           ref={ref}
           id={id}
