ac753aa34c9990c645230768d2dae215dcc209a5
André Barros <andrebarros7201@gmail.com>
2025-05-01 18:28:34 +0100

    Add notification slice to Redux store
    Integrated the notification slice into the Redux store to manage notification-related state. Also updated the dispatch type to `RootDispatch` for consistency.

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import notificationSlice from "@/redux/slices/notificationSlice";
 
-const store = configureStore({ reducer: {} });
+const store = configureStore({ reducer: { notification: notificationSlice } });
 
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootDispatch = typeof store.dispatch;
