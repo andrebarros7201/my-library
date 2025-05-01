16c68fee12421b142bf0304aef991d689f3784d3
André Barros <andrebarros7201@gmail.com>
2025-05-01 19:41:36 +0100

    Add input validation and notification to signup form
    Refactored the signup form to include validation for empty fields using `useRef`. Added a notification system to alert users when all fields are not filled. Also included a submit button to improve form accessibility.

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 import Form from "@/components/ui/Form";
 import Input from "@/components/ui/Input";
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
+import Button from "@/components/ui/Button";
+import createNotification from "@/utils/createNotification";
 
 const SignupPage = () => {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
+
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (!username || !password) {
+      createNotification({ type: "error", message: "All fields are required" });
+    }
   }
   return (
     <main className={"w-full flex flex-col justify-start items-center gap-4"}>
@@ -24,6 +36,7 @@ const SignupPage = () => {
           required={true}
           minLength={3}
         />
+        <Button label={"Create an account"} type={"submit"} />
       </Form>
     </main>
   );
