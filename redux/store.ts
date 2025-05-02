905a425d00adebe505f4f148dc042573c328ea8c
André Barros <andrebarros7201@gmail.com>
2025-05-02 15:40:30 +0100

    Add userSlice to Redux store configuration
    Integrated userSlice into the Redux store to manage user-related state. This enhances the application's state management capabilities and ensures better scalability for handling user data.

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notificationSlice from "@/redux/slices/notificationSlice";
+import userSlice from "@/redux/slices/userSlice";
 
-const store = configureStore({ reducer: { notification: notificationSlice } });
+const store = configureStore({
+  reducer: { notification: notificationSlice, user: userSlice },
+});
 
 export default store;
 
