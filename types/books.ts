05f80dc1f585f040566f0aab8cc54481b098f21a
André Barros <andrebarros7201@gmail.com>
2025-05-05 18:55:10 +0100

    Simplify and update book type definitions.
    Replaced old book-related interfaces with a streamlined structure for better alignment with the new API. Updated `Book` and `BookResponse` to reflect current data requirements, removing unused properties and interfaces for improved clarity and maintainability.

--- types/books.ts
+++ types/books.ts
@@ -1,80 +1,18 @@
-export interface IndustryIdentifier {
-  type: "ISBN_10" | "ISBN_13" | "ISSN" | "OTHER";
-  identifier: string;
-}
-
-export interface SaleInfo {
-  country: string;
-  saleability: "FOR_SALE" | "NOT_FOR_SALE" | "FREE" | "FOR_PREORDER";
-  isEbook: boolean;
-  listPrice?: {
-    amount: number;
-    currencyCode: string;
-  };
-}
-
-export interface ReadingModes {
-  text: boolean;
-  image: boolean;
-}
-
-export interface ImageLinks {
-  smallThumbnail?: string;
-  thumbnail?: string;
-  small?: string;
-  medium?: string;
-  large?: string;
-  extraLarge?: string;
-}
-
-export interface Dimensions {
-  height: string;
-  width: string;
-  thickness?: string;
-}
-
-export interface BookInfo {
-  title: string;
-  subtitle?: string;
-  authors?: string[];
-  publisher?: string;
-  publishedDate?: string;
-  description?: string;
-  industryIdentifiers?: IndustryIdentifier[];
-  pageCount?: number;
-  dimensions?: Dimensions;
-  printType?: string;
-  mainCategory: string;
-  categories?: string[];
-  averageRating?: number;
-  ratingsCount?: number;
-  imageLinks?: ImageLinks;
-  language?: string;
-  previewLink?: string;
-  infoLink?: string;
-  canonicalVolumeLink?: string;
-}
-
-export interface AccessInfo {
-  country: string;
-  viewability: "PARTIAL" | "ALL_PAGES" | "NO_PAGES" | "UNKNOWN";
-  embeddable: boolean;
-  publicDomain: boolean;
-  textToSpeechPermission: "ALLOWED" | "NOT_ALLOWED";
-}
-
 export interface Book {
-  kind: string;
-  id: string;
-  etag: string;
-  selfLink: string;
-  volumeInfo: BookInfo;
-  saleInfo?: SaleInfo;
-  accessInfo?: AccessInfo;
+  cover_i: number;
+  has_fulltext: string;
+  edition_count: number;
+  title: string;
+  author_name: string[];
+  first_publish_year: number;
+  key: string;
+  ia: string[];
+  author_key: string;
+  public_scan_b: boolean;
 }
 
-export interface BookSearchResponse {
-  kind: string;
-  totalItems: number;
-  items: Book[];
+export interface BookResponse {
+  docs: Book[];
+  start: number;
+  num_Found: number;
 }
