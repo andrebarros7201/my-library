69b6df53caadf3e86ad317e14af14307d3d95ec6
André Barros <andrebarros7201@gmail.com>
2025-05-13 18:14:21 +0100

    Enhance BookListItem with book cover image display
    Added a Next.js Image component to display book cover images in the BookListItem component. Improved the layout by structuring the title and author details alongside the image for better visual presentation.

--- components/BookListItem.tsx
+++ components/BookListItem.tsx
@@ -2,6 +2,7 @@ import { Book } from "@/types/books";
 import { RootDispatch } from "@/redux/store";
 import { useDispatch } from "react-redux";
 import { setCurrentBook } from "@/redux/slices/bookSlice";
+import Image from "next/image";
 
 type Props = {
   book: Book;
@@ -17,7 +18,18 @@ const BookListItem = ({ book, closeModal }: Props) => {
 
   return (
     <button className={"w-full cursor-pointer text-left"} onClick={handleClick}>
-      {book.title} by {book.author_name}
+      <div className={"flex gap-2"}>
+        <Image
+          src={book.imageURL_M}
+          alt={`Cover of ${book.title}`}
+          width={50}
+          height={75}
+        />
+        <div className={"grid grid-rows-2 gap-2"}>
+          <h3 className={"font-bold text-lg"}>{book.title}</h3>
+          <p>by {book.author_name}</p>
+        </div>
+      </div>
     </button>
   );
 };
