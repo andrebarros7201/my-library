a7e1299d712959d2bddadfe9044979c6848cde89
André Barros <andrebarros7201@gmail.com>
2025-05-13 17:05:41 +0100

    Add reusable Modal component
    Introduced a new `Modal` component for building modal dialogs. It supports custom child content and an optional `onClick` handler for background interaction. The component includes basic styling and layout for a centered modal.

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode | ReactNode[];
+  onClick?: () => void;
+};
+const Modal = ({ children, onClick }: Props) => {
+  return (
+    <div
+      className={
+        "w-screen h-screen absolute p-4 top-0 left-0 bg-black/70 flex gap-4 justify-center items-center"
+      }
+      onClick={onClick}
+    >
+      <div
+        className={"flex flex-col gap-4 bg-white w-full max-w-3xl rounded p-4"}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
