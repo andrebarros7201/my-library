8442ca11df0c657b49ae82352b19f3c58d53c786
André Barros <andrebarros7201@gmail.com>
2025-05-14 18:01:04 +0100

    Add AddCollection component to Library
    Introduce the AddCollection component alongside AddBook in the Library. Wrapped both components in a flex container for improved layout structure. This update enhances functionality by allowing users to add collections directly from the Library page.

--- components/library/Library.tsx
+++ components/library/Library.tsx
@@ -1,11 +1,15 @@
 import ProtectedRoute from "@/components/ProtectedRoute";
 import AddBook from "@/components/AddBook";
+import AddCollection from "@/components/AddCollection";
 
 const Library = () => {
   return (
     <ProtectedRoute>
       <main className={"w-full flex flex-col justify-start items-start gap-4"}>
-        <AddBook />
+        <div className={"w-full flex justify-start items-start gap-4"}>
+          <AddCollection />
+          <AddBook />
+        </div>
       </main>
     </ProtectedRoute>
   );
