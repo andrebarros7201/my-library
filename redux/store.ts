7843bb5d84fbd49a8c2b776be491c57ea0ac01bf
André Barros <andrebarros7201@gmail.com>
2025-05-14 18:48:26 +0100

    Add collectionSlice to the Redux store
    Integrated the collectionSlice into the Redux store to manage collection-related state. This ensures centralized state management for collections and aligns with the existing structure.

--- redux/store.ts
+++ redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import notificationSlice from "@/redux/slices/notificationSlice";
 import userSlice from "@/redux/slices/userSlice";
 import bookSlice from "@/redux/slices/bookSlice";
+import collectionSlice from "@/redux/slices/collectionSlice";
 
 const store = configureStore({
   reducer: {
     notification: notificationSlice,
     user: userSlice,
     book: bookSlice,
+    collection: collectionSlice,
   },
 });
 
