35b07e80e8f286897eb8aaf12f3df73a7f62e9bf
André Barros <andrebarros7201@gmail.com>
2025-05-14 18:33:42 +0100

    Update AddCollection form with validation and notifications
    Replaced title with name field and added client-side validation for required name input. Integrated notification system to inform users about missing fields, improving the collection creation process.

--- components/AddCollection.tsx
+++ components/AddCollection.tsx
@@ -6,16 +6,24 @@ import Button from "@/components/ui/Button";
 import Modal from "@/components/ui/Modal";
 import Form from "@/components/ui/Form";
 import Input from "@/components/ui/Input";
+import createNotification from "@/utils/createNotification";
 
 const AddCollection = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const titleRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
   const descriptionRef = useRef<HTMLInputElement>(null);
 
   const { user } = useSelector((state: RootState) => state.user);
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
+    const name = nameRef.current?.value;
+    const description = descriptionRef.current?.value;
+
+    if (!name) {
+      createNotification({ type: "error", message: "name is required" });
+      return;
+    }
   }
 
   return (
@@ -27,12 +35,13 @@ const AddCollection = () => {
       {isModalOpen && (
         <Modal onClick={() => setIsModalOpen(false)}>
           <Form onSubmit={handleSubmit}>
-            <Input label={"Title"} type={"text"} id={"title"} />
+            <Input label={"name"} type={"text"} id={"name"} ref={nameRef} />
             <Input
               label={"Description"}
               type={"text"}
               id={"description"}
               required={false}
+              ref={descriptionRef}
             />
             <Button label={"Create Collection"} type={"submit"} />
           </Form>
