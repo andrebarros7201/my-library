7a2fa8f434cd5de4a8bf6cd8f6f0344cdb9e3ead
André Barros <andrebarros7201@gmail.com>
2025-05-14 18:48:04 +0100

    Add Redux slice for managing collections
    Implemented a `collectionSlice` with state management for collections, including async creation of new collections. Added actions for clearing state and handling loading states with reducers to support API integration.

---
+++ redux/slices/collectionSlice.ts
@@ -0,0 +1,75 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { Collection } from "@/types/collection";
+import axios, { AxiosError } from "axios";
+
+interface CollectionState {
+  currentCollection: Collection | null;
+  collections: Collection[];
+  isLoading: boolean;
+}
+
+const initialState: CollectionState = {
+  currentCollection: null,
+  collections: [],
+  isLoading: false,
+};
+
+export const createCollection = createAsyncThunk(
+  "collection/create",
+  async (
+    {
+      name,
+      description = "",
+      userID,
+    }: { name: string; description?: string; userID: string },
+    { rejectWithValue },
+  ) => {
+    try {
+      const response = await axios.post("/api/collection", {
+        name,
+        description,
+        userID,
+      });
+      const { collection } = response.data;
+      return collection;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const { notification } = error.response!.data;
+        return rejectWithValue(notification.message);
+      } else {
+        return rejectWithValue("Something went wrong. Please try again later.");
+      }
+    }
+  },
+);
+
+const collectionSlice = createSlice({
+  name: "collection",
+  initialState,
+  reducers: {
+    clearState: (state) => {
+      state.currentCollection = null;
+      state.collections = [];
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCollection.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createCollection.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.collections.push(action.payload);
+        if (state.currentCollection == null) {
+          state.currentCollection = action.payload;
+        }
+      })
+      .addCase(createCollection.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+});
+
+export default collectionSlice.reducer;
+export const { clearState } = collectionSlice.actions;
