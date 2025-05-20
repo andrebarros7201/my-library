e8dba35a762c8bc7d0f6fab672ac76fd36a10f93
André Barros <andrebarros7201@gmail.com>
2025-05-20 20:33:06 +0100

    Handle book addition with user validation and collection checks
    Added validation to ensure the collection exists before adding a book. Checked if the user already owns the book; if so, linked it to the collection instead of creating a duplicate. Adjusted logic to maintain data integrity and return appropriate success or error responses.

--- app/api/collection/[collectionID]/book/route.ts
+++ app/api/collection/[collectionID]/book/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
 
-//TODO check if user has a collection with this book
-
-// ADD A NEW BOOK TO COLLECTION
+// ADD A NEW BOOK TO CURRENT COLLECTION
 export async function POST(
   req: NextRequest,
   context: { params: Promise<{ collectionID: string }> },
@@ -12,6 +10,19 @@ export async function POST(
     const { collectionID } = await context.params;
     const { book } = await req.json();
 
+    // Fetch collection
+    const collection = await prisma.collection.findUnique({
+      where: { id: collectionID },
+    });
+
+    // If collection was not found return error notification
+    if (!collection)
+      return NextResponse.json(
+        { notification: { type: "error", message: "Collection not found" } },
+        { status: 404 },
+      );
+
+    // Check if book was not provided
     if (!book) {
       return NextResponse.json(
         { notification: { type: "error", message: "Failed to add book" } },
@@ -19,9 +30,51 @@ export async function POST(
       );
     }
 
+    // Check if user already has this book
+    const userHasBook = await prisma.book.findFirst({
+      where: {
+        AND: [
+          {
+            userID: collection.userID,
+          },
+          {
+            key: book.key,
+          },
+        ],
+      },
+    });
+
+    // If it does, connect that book with the current collection
+    if (userHasBook) {
+      await prisma.collection.update({
+        where: { id: collectionID },
+        data: { books: { connect: { id: userHasBook.id } } },
+      });
+
+      const updatedCollection = await prisma.collection.findUnique({
+        where: { id: collectionID },
+        include: {
+          books: true,
+        },
+      });
+
+      return NextResponse.json(
+        {
+          notification: {
+            type: "success",
+            message: "Book Added to Collection",
+          },
+          collection: updatedCollection,
+        },
+        { status: 200 },
+      );
+    }
+
     const newBook = await prisma.book.create({
       data: {
+        userID: collection.userID,
         cover_i: book.cover_i,
+        key: book.key,
         imageURL_S: `https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`,
         imageURL_M: `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`,
         imageURL_L: `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`,
