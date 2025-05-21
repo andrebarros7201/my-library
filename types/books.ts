a2e8f0ae784e580b9490509ff635589907f62d95
André Barros <andrebarros7201@gmail.com>
2025-05-21 10:40:46 +0100

    Make `cover_i` and `first_publish_year` optional
    Updated the `cover_i` and `first_publish_year` fields in the `Book` model and TypeScript interface to be optional. This change improves flexibility for records where these fields may not always be available.

--- types/books.ts
+++ types/books.ts
@@ -1,13 +1,13 @@
 export interface Book {
   userID: string;
-  cover_i: number;
+  cover_i?: number;
   imageURL_S: string;
   imageURL_M: string;
   imageURL_L: string;
   edition_count: number;
   title: string;
   author_name: string[];
-  first_publish_year: number;
+  first_publish_year?: number;
   key: string;
   status: string;
 }
