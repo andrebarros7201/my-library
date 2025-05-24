56b5a547d85f65ca9f05a2d3bc73202495b54f61
André Barros <andrebarros7201@gmail.com>
2025-05-24 21:28:43 +0100

    test: enhance Button component tests with additional scenarios

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -1,10 +1,46 @@
 import Button from "@/components/ui/Button";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 describe("Button Component", () => {
   it("should render with the correct label", () => {
     render(<Button label="Test" />);
     expect(screen.getByText("Test")).toBeInTheDocument();
+  });
+
+  it("should be disabled", () => {
+    render(<Button disabled={true} label="Disabled Button" />);
+    expect(screen.getByText("Disabled Button")).toBeDisabled();
+  });
+
+  it("should be of type submit", () => {
+    render(<Button label="Submit Button" type="submit" />);
+    expect(screen.getByText("Submit Button")).toHaveAttribute("type", "submit");
+  });
+
+  it("should call onClick when button is pressed", async () => {
+    const onClickFn = jest.fn();
+    render(<Button label="Click Me" onClick={onClickFn} />);
+
+    const button = screen.getByText("Click Me");
+    await userEvent.click(button);
+    expect(onClickFn).toHaveBeenCalled();
+  });
+
+  it("should not call onClick if button is disabled", async () => {
+    const onClickFn = jest.fn();
+    render(<Button label="Click Me" onClick={onClickFn} disabled={true} />);
+
+    const button = screen.getByText("Click Me");
+    await fireEvent.click(button);
+
+    expect(onClickFn).not.toHaveBeenCalled();
+  });
+
+  it("should have correct class for primary variant", () => {
+    render(<Button label="Primary" variant="primary" />);
+    const button = screen.getByText("Primary");
+    expect(button).toHaveClass("bg-blue-500");
   });
 });
