337dc70a37a8934e22c3e7096d67c7e154f70f6a
André Barros <andrebarros7201@gmail.com>
2025-05-24 18:31:22 +0100

    chore: add jest for testing and update dependencies
    - Added "test" script to package.json for running jest tests.
    - Updated dependencies: added jest-environment-jsdom, ts-node, @testing-library/jest-dom, @testing-library/user-event, and @types/jest.
    - Updated devDependencies to include ts-jest for TypeScript support in jest.

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default config;
